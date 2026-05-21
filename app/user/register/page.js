ad0fc64d1e411b1879689aef44a2586f62a076f2
Blon Tamang Bipin <251019@st.yoshida-g.ac.jp>
2026-05-21 13:56:29 +0900

    finished

--- app/user/register/page.js
+++ app/user/register/page.js
@@ -1,11 +1,12 @@
-import { title } from "process"
-import myPage from "./myPage"
+import MyPage from "./MyPage"
+
 export const metadata = {
     title: "登録ページ",
     description: "これは登録ページです",
 }
 
-const Register = () =>{
-    return <myPage/>
+const Register = () => {
+    return <MyPage/>
 }
+
 export default Register
