a0ed3e7f3a914403a2c4e6163e0a37ce202ca4e4
Blon Tamang Bipin <251019@st.yoshida-g.ac.jp>
2026-05-25 13:11:18 +0900

    error

--- app/user/register/MyPage.js
+++ app/user/register/MyPage.js
@@ -1,7 +1,7 @@
 "use client"
 import { useState } from "react"
 
-const Register = () => {
+const MyPage = () => {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
@@ -9,23 +9,18 @@ const Register = () => {
     const handleSubmit = async(e) => {
         e.preventDefault()
         try{
-            const response = await fetch("http://localhost:3000/api/user/register",{
+            const response = await fetch("/api/user/register", {
                 method: "POST",
                 headers:{
                     "Accept": "application/json",
                     "Content-Type": "application/json"
                 }, 
-                body: JSON.stringify({
-                    name:name,
-                    email: email,
-                    password: password
-                })
-            } )
+                body: JSON.stringify({name, email, password})
+            })
             const jsonData = await response.json()
             alert(jsonData.message)
         }catch(err){
-            console.error(err);
-            
+            console.error(err)
             alert("ユーザー登録失敗")
         }
     }
@@ -33,13 +28,12 @@ const Register = () => {
         <div>
             <h1>ユーザー登録</h1>
             <form onSubmit={handleSubmit}>
-                <input value={name} onChange={(e) => setName(e.target.value)}  type="text"  name="name" placeholder="名前" required />
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="text"  name="email" placeholder="メールアドレス" required />
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="text"  name="password" placeholder="パスワード" required />
+                <input value={name} onChange={(e) => setName(e.target.value)} type="text" name="name" placeholder="名前" required />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" name="email" placeholder="メールアドレス" required />
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="text" name="password" placeholder="パスワード" required />
                 <button>登録</button>
-
             </form>
         </div>
     )
 }
-export default Register
+export default MyPage
